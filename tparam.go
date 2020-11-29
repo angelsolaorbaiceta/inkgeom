054939766777438307559c5bfc32163b4badc16b
Angel Sola <angelsolaorbaiceta@gmail.com>
2020-11-29 11:06:19 +0100

    Separate sorting from tparam main file

--- tparam.go
+++ tparam.go
@@ -127,25 +127,6 @@ func (t TParam) Value() float64 {
 	return t.value
 }
 
-/* <-- sort.Interface --> */
-
-/*
-ByTParamValue implements sort.Interface for []TParam based on the value field.
-*/
-type ByTParamValue []TParam
-
-func (a ByTParamValue) Len() int {
-	return len(a)
-}
-
-func (a ByTParamValue) Swap(i, j int) {
-	a[i], a[j] = a[j], a[i]
-}
-
-func (a ByTParamValue) Less(i, j int) bool {
-	return a[i].value < a[j].value
-}
-
 /* <-- Functions --> */
 
 /*
