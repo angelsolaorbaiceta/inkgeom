30c4c8abe1a09601d6c05d4d353cb4c69de64b80
Angel <angelsolaorbaiceta@gmail.com>
2021-11-24 14:28:11 +0100

    test (g3d): test line's PointAt method

--- g3d/line_test.go
+++ g3d/line_test.go
@@ -1,6 +1,9 @@
 package g3d
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestCreateLine(t *testing.T) {
 	var (
@@ -17,4 +20,26 @@ func TestCreateLine(t *testing.T) {
 	if got := line.Direction(); !wantDirection.Equals(got) {
 		t.Errorf("Want line direction %v, but got %v", wantDirection, got)
 	}
+}
+
+func TestPointAt(t *testing.T) {
+	var (
+		origin = MakePoint(1, 2, 3)
+		dir    = MakeVersor(1, 1, 1)
+		line   = MakeLine(origin, dir)
+	)
+
+	t.Run("At t = 0, the point is the line's origin", func(t *testing.T) {
+		if got := line.PointAt(0.0); !origin.Equals(got) {
+			t.Errorf("Want point %v, but got %v", origin, got)
+		}
+	})
+
+	t.Run("Get point at t > 0", func(t *testing.T) {
+		want := MakePoint(2, 3, 4)
+
+		if got := line.PointAt(math.Sqrt(3)); !want.Equals(got) {
+			t.Errorf("Want point %v, but got %v", want, got)
+		}
+	})
 }
