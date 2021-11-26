4a68a308d08b3e32becb0868b8c6dbc975d40086
Angel <angelsolaorbaiceta@gmail.com>
2021-11-26 19:36:58 +0100

    test (nums): test nums close to one and zero

--- nums/numbers.go
+++ nums/numbers.go
@@ -4,18 +4,14 @@ import "math"
 
 const defaultEpsilon = 1e-10
 
-/*
-FloatsEqualEps compares two float64 values and returns true if the difference between
-the two is smaller than a given epsilon.
-*/
+// FloatsEqualEps compares two float64 values and returns true if the difference between
+// the two is smaller than a given epsilon.
 func FloatsEqualEps(a, b, epsilon float64) bool {
 	return math.Abs(a-b) < epsilon
 }
 
-/*
-FloatsEqual compares two float64 values and returns true if the difference between the
-two is smaller than a default epsilon value (1E-10).
-*/
+// FloatsEqual compares two float64 values and returns true if the difference between the
+// two is smaller than a default epsilon value (1E-10).
 func FloatsEqual(a, b float64) bool {
 	return FloatsEqualEps(a, b, defaultEpsilon)
 }
@@ -30,10 +26,8 @@ func IsCloseToZero(a float64) bool {
 	return FloatsEqual(a, 0.0)
 }
 
-/*
-LinInterpol computes the linear interpolation for a given position given two points on
-the desired line: (startPos, startVal) and (endPos, endVal).
-*/
+// LinInterpol computes the linear interpolation for a given position given two points on
+// the desired line: (startPos, startVal) and (endPos, endVal).
 func LinInterpol(startPos, startVal, endPos, endVal, posToInterpolate float64) float64 {
 	return startVal + (posToInterpolate-startPos)*(endVal-startVal)/(endPos-startPos)
 }
--- nums/numbers_test.go
+++ nums/numbers_test.go
@@ -16,6 +16,34 @@ func TestFloatsEqual(t *testing.T) {
 	})
 }
 
+func TestNumberCloseToOne(t *testing.T) {
+	t.Run("is true if number is 1.0", func(t *testing.T) {
+		if !IsCloseToOne(1.0) {
+			t.Error("Expected number to be 1.0")
+		}
+	})
+
+	t.Run("is false if number isn't 1.0", func(t *testing.T) {
+		if IsCloseToOne(234.98) {
+			t.Error("Expected number to not be 1.0")
+		}
+	})
+}
+
+func TestNumberCloseToZero(t *testing.T) {
+	t.Run("is true if number is 0.0", func(t *testing.T) {
+		if !IsCloseToZero(0.0) {
+			t.Error("Expected number to be 1.0")
+		}
+	})
+
+	t.Run("is false if number isn't 0.0", func(t *testing.T) {
+		if IsCloseToZero(234.98) {
+			t.Error("Expected number to not be 0.0")
+		}
+	})
+}
+
 func TestLinearInterpolation(t *testing.T) {
 	var (
 		want = 10.0
