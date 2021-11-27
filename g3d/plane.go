6555316116316315f620a464c01019ae760b5fb9
Angel <angelsolaorbaiceta@gmail.com>
2021-11-27 11:37:03 +0100

    feat (g3d): create plane from point and normal vector

--- g3d/plane.go
+++ g3d/plane.go
@@ -1,15 +1,17 @@
 package g3d
 
+import "github.com/angelsolaorbaiceta/inkgeom/nums"
+
 // A Plane is a three dimensional straight surface defined by a point and a normal versor.
 // The plane is defined in its ax + by + cz + d = 0 form.
 type Plane struct {
-	a, b, c, d   float64
+	d            float64
 	normalVector *Vector
 }
 
 // MakePlane creates a new plane give its a, b, c, and d components of the equation
 // ax + by + cz + d = 0.
-// Returns an error if the resulting normal vector has zero length.
+// Returns a ErrZeroVector if the resulting normal vector has zero length.
 func MakePlane(a, b, c, d float64) (*Plane, error) {
 	normalVec, err := MakeNonZeroVector(a, b, c)
 
@@ -17,11 +19,46 @@ func MakePlane(a, b, c, d float64) (*Plane, error) {
 		return nil, err
 	}
 
-	return &Plane{a, b, c, d, normalVec}, nil
+	return &Plane{d, normalVec}, nil
+}
+
+// MakePlaneFromPointAndNormal returns a new plane passing through a given point and whose normal
+// direction is given by the passed in vector.
+// Returns a ErrZeroVector if the resulting normal vector has zero length.
+func MakePlaneFromPointAndNormal(p *Point, normal *Vector) (*Plane, error) {
+	if nums.IsCloseToZero(normal.Length()) {
+		return nil, ErrZeroVector
+	}
+
+	return &Plane{
+		d:            -(p.x*normal.x + p.y*normal.y + p.z*normal.z),
+		normalVector: normal,
+	}, nil
+}
+
+func (p *Plane) a() float64 {
+	return p.normalVector.x
+}
+
+func (p *Plane) b() float64 {
+	return p.normalVector.y
+}
+
+func (p *Plane) c() float64 {
+	return p.normalVector.z
 }
 
 // The NormalVector is a vector normal to the surface of the plane.
 // Its projections are given by the a, b and c parameters in the plane's equation.
 func (p *Plane) NormalVector() *Vector {
 	return p.normalVector
+}
+
+// ContainsPoint checks whether the given point is on the plane.
+func (p *Plane) ContainsPoint(pt *Point) bool {
+	return nums.IsCloseToZero(p.evaluatePoint(pt) + p.d)
+}
+
+func (p *Plane) evaluatePoint(pt *Point) float64 {
+	return p.a()*pt.x + p.b()*pt.y + p.c()*pt.z
 }
--- g3d/plane_test.go
+++ g3d/plane_test.go
@@ -5,13 +5,19 @@ import "testing"
 func TestPlaneCreationFromParams(t *testing.T) {
 	t.Run("from the a, b, c and d parameters", func(t *testing.T) {
 		var (
-			a        = 1.0
-			b        = 2.0
-			c        = 3.0
-			d        = 4.0
-			plane, _ = MakePlane(a, b, c, d)
-			want     = MakeVector(a, b, c)
+			a          = 1.0
+			b          = 2.0
+			c          = 3.0
+			d          = 4.0
+			plane, err = MakePlane(a, b, c, d)
+			want       = MakeVector(a, b, c)
 		)
+
+		t.Run("plane created without error", func(t *testing.T) {
+			if err != nil {
+				t.Errorf("Want no error, but got %v", err)
+			}
+		})
 
 		t.Run("computes the normal vector", func(t *testing.T) {
 			if got := plane.NormalVector(); !want.Equals(got) {
@@ -29,5 +35,42 @@ func TestPlaneCreationFromParams(t *testing.T) {
 				t.Error("Expected error")
 			}
 		})
+	})
+}
+
+func TestPlaneCreationFromPointAndNormal(t *testing.T) {
+	var (
+		p          = MakePoint(10, 20, 30)
+		n          = MakeVector(1, 1, 1)
+		plane, err = MakePlaneFromPointAndNormal(p, n)
+	)
+
+	t.Run("plane created without error", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Want no error, but got %v", err)
+		}
+	})
+
+	t.Run("uses the normal vector", func(t *testing.T) {
+		if got := plane.NormalVector(); !n.Equals(got) {
+			t.Errorf("Want normal vector %v, but got %v", n, got)
+		}
+	})
+
+	t.Run("contains the point", func(t *testing.T) {
+		if !plane.ContainsPoint(p) {
+			t.Error("Expected plane to contain point")
+		}
+	})
+
+	t.Run("can't create with a zero length normal vector", func(t *testing.T) {
+		plane, err := MakePlaneFromPointAndNormal(p, Zero)
+
+		if plane != nil {
+			t.Errorf("Want nil plane, got %v", plane)
+		}
+		if err != ErrZeroVector {
+			t.Error("Expected error")
+		}
 	})
 }
