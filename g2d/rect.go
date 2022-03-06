0594a4d2682d6b703b90ee8d01d3bfdc250df5ae
angel <angelsolaorbaiceta@gmail.com>
2022-03-06 19:16:45 +0100

    Rect contains point

--- g2d/rect.go
+++ g2d/rect.go
@@ -1,5 +1,7 @@
 package g2d
 
+import "fmt"
+
 // A Rect is a two dimensional rectangle whose edges are horizontal and vertical.
 // A Rect is defined by an origin point and two numbers: the width and height.
 type Rect struct {
@@ -30,4 +32,38 @@ func (r *Rect) Width() float64 {
 // The Height of the rectangle: the vertical dimension.
 func (r *Rect) Height() float64 {
 	return r.height
+}
+
+// Left is the left-most X coordinate of the rectangle.
+func (r *Rect) Left() float64 {
+	return r.origin.x
+}
+
+// Right is the right-most X coordinate of the rectangle.
+func (r *Rect) Right() float64 {
+	return r.origin.x + r.width
+}
+
+// Bottom is the low-most Y coordinate of the rectangle.
+func (r *Rect) Bottom() float64 {
+	return r.origin.y
+}
+
+// Top is the top-most Y coordinate of the rectangle.
+func (r *Rect) Top() float64 {
+	return r.origin.y + r.height
+}
+
+// ContainsPoint checks whether this rectangle contains the given point.
+// Edges are not considered part of the rectangle, so a point on an edge is considered to be
+// outside of the rectangle.
+func (r *Rect) ContainsPoint(point *Point) bool {
+	return point.x > r.Left() &&
+		point.x < r.Right() &&
+		point.y > r.Bottom() &&
+		point.y < r.Top()
+}
+
+func (r *Rect) String() string {
+	return fmt.Sprintf("Rect{origin: %v, size: (%f, %f)}", &r.origin, r.width, r.height)
 }
--- g2d/rect_test.go
+++ g2d/rect_test.go
@@ -19,4 +19,40 @@ func TestCreateRect(t *testing.T) {
 	if got := rect.Height(); got != height {
 		t.Errorf("Want height %f, got %f", got, height)
 	}
+
+	if got := rect.Left(); got != origin.x {
+		t.Errorf("Want left %f, got %f", origin.x, got)
+	}
+	if got := rect.Right(); got != origin.x+width {
+		t.Errorf("Want right %f, got %f", origin.x+width, got)
+	}
+	if got := rect.Bottom(); got != origin.y {
+		t.Errorf("Want bottom %f, got %f", origin.y, got)
+	}
+	if got := rect.Top(); got != origin.y+height {
+		t.Errorf("Want top %f, got %f", origin.y+height, got)
+	}
+}
+
+func TestRectContainsPoint(t *testing.T) {
+	var (
+		rect          = MakeRect(*MakePoint(10, 20), 100, 200)
+		pointInside   = MakePoint(50, 70)
+		pointsOutside = []*Point{
+			MakePoint(5, 70),
+			MakePoint(50, 300),
+			MakePoint(150, 70),
+			MakePoint(50, 5),
+		}
+	)
+
+	if !rect.ContainsPoint(pointInside) {
+		t.Errorf("Expected %v to cointain point %v", rect, pointInside)
+	}
+
+	for _, point := range pointsOutside {
+		if rect.ContainsPoint(point) {
+			t.Errorf("Expected %v not to cointain point %v", rect, point)
+		}
+	}
 }
