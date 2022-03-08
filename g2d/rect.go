c4489f5bc1b08ba8ccdd53962f93ef7a82e940f6
Angel <angelsolaorbaiceta@gmail.com>
2022-03-08 18:44:19 +0100

    Feat: Add rectangle primitive (#2)

---
+++ g2d/rect.go
@@ -0,0 +1,141 @@
+package g2d
+
+import (
+	"errors"
+	"fmt"
+	"math"
+
+	"github.com/angelsolaorbaiceta/inkgeom/nums"
+)
+
+// A Rect is a two dimensional rectangle whose edges are horizontal and vertical.
+// A Rect is defined by an origin point and two numbers: the width and height.
+type Rect struct {
+	origin *Point
+	width  float64
+	height float64
+}
+
+// MakeRect creates a new rectangle given an origin point, the width and height.
+//
+// A non-nil error is returned if either the width or height are smaller than zero.
+func MakeRect(origin *Point, width, height float64) (*Rect, error) {
+	if width < 0.0 {
+		return nil, errors.New("the width must be greater or equal to zero")
+	}
+
+	if height < 0.0 {
+		return nil, errors.New("the height must be greater or equal to zero")
+	}
+
+	return &Rect{
+		origin: origin,
+		width:  width,
+		height: height,
+	}, nil
+}
+
+// MakeRectContaining returns the smallest rectangle containing all the given points.
+// Some of the input points end up in one of the edges, thus, those points won't pass the
+// ContainsPoint test.
+//
+// A non-nil error is returned if the list of points is empty.
+func MakeRectContaining(points []*Point) (*Rect, error) {
+	if len(points) < 1 {
+		return nil, errors.New("at least one point is required to construct the rectangle")
+	}
+
+	var (
+		firstPoint = points[0]
+		point      *Point
+		minX       = firstPoint.x
+		maxX       = firstPoint.x
+		minY       = firstPoint.y
+		maxY       = firstPoint.y
+	)
+
+	for i := 1; i < len(points); i++ {
+		point = points[i]
+
+		minX = math.Min(minX, point.x)
+		maxX = math.Max(maxX, point.x)
+		minY = math.Min(minY, point.y)
+		maxY = math.Max(maxY, point.y)
+	}
+
+	return MakeRect(
+		MakePoint(minX, minY),
+		maxX-minX,
+		maxY-minY,
+	)
+}
+
+// The Origin of the rectangle
+func (r *Rect) Origin() *Point {
+	return r.origin
+}
+
+// The Width of the rectangle: the horizontal dimension.
+func (r *Rect) Width() float64 {
+	return r.width
+}
+
+// The Height of the rectangle: the vertical dimension.
+func (r *Rect) Height() float64 {
+	return r.height
+}
+
+// Left is the left-most X coordinate of the rectangle.
+func (r *Rect) Left() float64 {
+	return r.origin.x
+}
+
+// Right is the right-most X coordinate of the rectangle.
+func (r *Rect) Right() float64 {
+	return r.origin.x + r.width
+}
+
+// Bottom is the low-most Y coordinate of the rectangle.
+func (r *Rect) Bottom() float64 {
+	return r.origin.y
+}
+
+// Top is the top-most Y coordinate of the rectangle.
+func (r *Rect) Top() float64 {
+	return r.origin.y + r.height
+}
+
+// ContainsPoint checks whether this rectangle contains the given point.
+// Edges are not considered part of the rectangle, so a point on an edge is considered to be
+// outside of the rectangle.
+func (r *Rect) ContainsPoint(point *Point) bool {
+	return point.x > r.Left() &&
+		point.x < r.Right() &&
+		point.y > r.Bottom() &&
+		point.y < r.Top()
+}
+
+// WithScaledSize creates a new rectangle resulting from scaling the width and height of this one.
+func (r *Rect) WithScaledSize(scale float64) (*Rect, error) {
+	return MakeRect(r.origin, r.width*scale, r.height*scale)
+}
+
+// WithMargins creates a new rectangle resulting of adding margins to this one.
+func (r *Rect) WithMargins(lateral, vertical float64) (*Rect, error) {
+	return MakeRect(
+		MakePoint(r.origin.x-lateral, r.origin.y-vertical),
+		2*lateral+r.width,
+		2*vertical+r.height,
+	)
+}
+
+// Equals checks if this and other rectangle are equal.
+func (r *Rect) Equals(other *Rect) bool {
+	return r.origin.Equals(other.origin) &&
+		nums.FloatsEqual(r.width, other.width) &&
+		nums.FloatsEqual(r.height, other.height)
+}
+
+func (r *Rect) String() string {
+	return fmt.Sprintf("Rect{origin: %v, size: (%f, %f)}", &r.origin, r.width, r.height)
+}
---
+++ g2d/rect_test.go
@@ -0,0 +1,140 @@
+package g2d
+
+import "testing"
+
+func TestCreateRect(t *testing.T) {
+	var (
+		origin  = MakePoint(2, 3)
+		width   = 20.0
+		height  = 30.0
+		rect, _ = MakeRect(origin, width, height)
+	)
+
+	if got := rect.Origin(); !got.Equals(origin) {
+		t.Errorf("Want origin %v, got %v", origin, got)
+	}
+	if got := rect.Width(); got != width {
+		t.Errorf("Want width %f, got %f", got, width)
+	}
+	if got := rect.Height(); got != height {
+		t.Errorf("Want height %f, got %f", got, height)
+	}
+
+	if got := rect.Left(); got != origin.x {
+		t.Errorf("Want left %f, got %f", origin.x, got)
+	}
+	if got := rect.Right(); got != origin.x+width {
+		t.Errorf("Want right %f, got %f", origin.x+width, got)
+	}
+	if got := rect.Bottom(); got != origin.y {
+		t.Errorf("Want bottom %f, got %f", origin.y, got)
+	}
+	if got := rect.Top(); got != origin.y+height {
+		t.Errorf("Want top %f, got %f", origin.y+height, got)
+	}
+
+	t.Run("Returns an error if the width is less than zero", func(t *testing.T) {
+		if rect, err := MakeRect(origin, -width, height); err == nil || rect != nil {
+			t.Error("Want error, got nil")
+		}
+	})
+
+	t.Run("Returns an error if the height is less than zero", func(t *testing.T) {
+		if rect, err := MakeRect(origin, width, -height); err == nil || rect != nil {
+			t.Error("Want error, got nil")
+		}
+	})
+}
+
+func TestMakeRectContainingPoints(t *testing.T) {
+	var (
+		points = []*Point{
+			MakePoint(0, 5),
+			MakePoint(10, 0),
+			MakePoint(5, 7),
+		}
+		wantRect, _ = MakeRect(MakePoint(0, 0), 10, 7)
+	)
+
+	if got, _ := MakeRectContaining(points); !got.Equals(wantRect) {
+		t.Errorf("Want %v, got %v", wantRect, got)
+	}
+}
+
+func TestRectContainsPoint(t *testing.T) {
+	var (
+		rect, _       = MakeRect(MakePoint(10, 20), 100, 200)
+		pointInside   = MakePoint(50, 70)
+		pointsOutside = []*Point{
+			MakePoint(5, 70),
+			MakePoint(50, 300),
+			MakePoint(150, 70),
+			MakePoint(50, 5),
+		}
+	)
+
+	if !rect.ContainsPoint(pointInside) {
+		t.Errorf("Expected %v to cointain point %v", rect, pointInside)
+	}
+
+	for _, point := range pointsOutside {
+		if rect.ContainsPoint(point) {
+			t.Errorf("Expected %v not to cointain point %v", rect, point)
+		}
+	}
+}
+
+func TestScaleRect(t *testing.T) {
+	var (
+		origin      = MakePoint(2, 3)
+		width       = 20.0
+		height      = 30.0
+		scale       = 5.0
+		rect, _     = MakeRect(origin, width, height)
+		wantRect, _ = MakeRect(origin, width*scale, height*scale)
+	)
+
+	if got, _ := rect.WithScaledSize(scale); !got.Equals(wantRect) {
+		t.Errorf("Want %v, got %v", wantRect, got)
+	}
+}
+
+func TestRectWithMargin(t *testing.T) {
+	rect, _ := MakeRect(MakePoint(0, 0), 100, 200)
+
+	t.Run("compute a larger rect", func(t *testing.T) {
+		var (
+			lateralMargin  = 10.0
+			verticalMargin = 20.0
+			wantRect, _    = MakeRect(MakePoint(-10, -20), 120, 240)
+		)
+
+		if got, _ := rect.WithMargins(lateralMargin, verticalMargin); !got.Equals(wantRect) {
+			t.Errorf("Want %v, got %v", wantRect, got)
+		}
+	})
+
+	t.Run("compute a smaller rect", func(t *testing.T) {
+		var (
+			lateralMargin  = -10.0
+			verticalMargin = -20.0
+			wantRect, _    = MakeRect(MakePoint(10, 20), 80, 160)
+		)
+
+		if got, _ := rect.WithMargins(lateralMargin, verticalMargin); !got.Equals(wantRect) {
+			t.Errorf("Want %v, got %v", wantRect, got)
+		}
+	})
+
+	t.Run("can't create a rect with negative width", func(t *testing.T) {
+		if got, err := rect.WithMargins(-51, 0); got != nil || err == nil {
+			t.Error("Want error")
+		}
+	})
+
+	t.Run("can't create a rect with negative height", func(t *testing.T) {
+		if got, err := rect.WithMargins(0, -101); got != nil || err == nil {
+			t.Error("Want error")
+		}
+	})
+}
