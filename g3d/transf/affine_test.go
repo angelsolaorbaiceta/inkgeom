cccd8cfc71495bda6281569c157545869171427b
angel.sola <angel.sola@glovoapp.com>
2022-09-29 12:49:31 +0200

    test: Test rotation affine transformations

--- g3d/transf/affine_test.go
+++ g3d/transf/affine_test.go
@@ -22,6 +22,35 @@ func TestAffineTransformation(t *testing.T) {
 		)
 
 		got := rotation.Apply(original)
+
+		assert.True(got.Equals(want))
+	})
+
+	t.Run("Rotation in the XZ plane", func(t *testing.T) {
+		var (
+			angle    = math.Pi / 2
+			origin   = g3d.MakePoint(4, 0, 5)
+			rotation = MakeRotationAround(angle, g3d.JVersor, origin)
+			original = g3d.MakeVector(7, 0, 5)
+			want     = g3d.MakeVector(4, 0, 2)
+		)
+
+		got := rotation.Apply(original)
+		fmt.Println(got)
+
+		assert.True(got.Equals(want))
+	})
+
+	t.Run("Rotation in the XY plane", func(t *testing.T) {
+		var (
+			angle    = math.Pi / 2
+			origin   = g3d.MakePoint(4, 5, 0)
+			rotation = MakeRotationAround(angle, g3d.KVersor, origin)
+			original = g3d.MakeVector(7, 5, 0)
+			want     = g3d.MakeVector(4, 8, 0)
+		)
+
+		got := rotation.Apply(original)
 		fmt.Println(got)
 
 		assert.True(got.Equals(want))
